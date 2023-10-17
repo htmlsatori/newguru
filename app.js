c50b9d8a1089b4feec2f761bd4fef9dbdf403448
htmlsatori <72777953+htmlsatori@users.noreply.github.com>
2023-10-17 12:38:20 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("full-screen-menu").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("full-screen-menu").style.height = "0%";
+}
